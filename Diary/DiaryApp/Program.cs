d414ca0c4972008dccd127e19a04c514e0a1ed28
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-10 16:39:12 +0100

    Added webpage

--- Diary/DiaryApp/Program.cs
+++ Diary/DiaryApp/Program.cs
@@ -14,12 +14,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
+// Default view on a startup
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
