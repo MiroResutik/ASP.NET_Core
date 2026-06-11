6b73ee805b3e8b9912cb25c02ac3a9a58aed3a1d
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-11 16:26:48 +0100

    Controler added for Diary entry + CSS styling change

---
+++ Diary/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,36 @@
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiaryApp.Controllers
+{
+    public class DiaryEntriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        // Dependency Injection - injecting ApplicationDbContext
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
+
+            return View(objDiaryEntryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(DiaryEntry obj)
+        {
+            // Add object  Diary Entries to database
+            _db.DiaryEntries.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Diary/DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        // Properties
+        //[Key] 
+        public int DiaryEntryId { get; set; }
+        [Required] // cannot be null
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Today; 
+
+    }
+}
