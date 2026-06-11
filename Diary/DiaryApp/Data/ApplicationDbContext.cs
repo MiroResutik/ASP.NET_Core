038c8ca2793a11f2dc9c51191eaaffd5e0e12e18
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-11 16:25:58 +0100

    Setting up and adding data to database

---
+++ Diary/DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Create a Table of type DiaryEntries 
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        // Method to seed the data into a database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry 
+                { 
+                    DiaryEntryId = 1, 
+                    Title="Went Hiking", 
+                    Content="Went hiking with Miro!",
+                    Created = DateTime.Today
+                },
+                new DiaryEntry
+                {
+                    DiaryEntryId = 2,
+                    Title = "Went Biking",
+                    Content = "Went biking with Miro!",
+                    Created = DateTime.Today
+                },
+                new DiaryEntry
+                {
+                    DiaryEntryId = 3,
+                    Title = "Went Shoping",
+                    Content = "Went Shoping with Miro!",
+                    Created = DateTime.Today
+                },
+                new DiaryEntry
+                {
+                    DiaryEntryId = 4,
+                    Title = "Went Diving",
+                    Content = "Went Diving with Miro!",
+                    Created = DateTime.Today
+                }
+                );
+        }
+    }
+}
---
+++ Diary/DiaryApp/Migrations/20260611112701_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 12, 20, 54, 197, DateTimeKind.Local).AddTicks(3014));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 12, 20, 54, 198, DateTimeKind.Local).AddTicks(6584));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 12, 20, 54, 198, DateTimeKind.Local).AddTicks(6597));
+
+            migrationBuilder.UpdateData(
+                table: "DiaryEntries",
+                keyColumn: "DiaryEntryId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2026, 6, 11, 12, 20, 54, 198, DateTimeKind.Local).AddTicks(6599));
+        }
+    }
+}
