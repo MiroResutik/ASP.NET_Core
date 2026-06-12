85c14e036bc5b89b42c0691dee8c597efcec8325
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-12 10:58:49 +0100

    Added CRUD operations

--- Diary/DiaryApp/Controllers/DiaryEntriesController.cs
+++ Diary/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -27,10 +27,83 @@ namespace DiaryApp.Controllers
         [HttpPost]
         public IActionResult Create(DiaryEntry obj)
         {
-            // Add object  Diary Entries to database
-            _db.DiaryEntries.Add(obj);
-            _db.SaveChanges();
+            // Server side validation
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short!!! (Server side validation)");
+            }
+            if (ModelState.IsValid) 
+            {
+                // Add object  Diary Entries to database
+                _db.DiaryEntries.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj);  // Update the diary entry in the database
+                _db.SaveChanges();// Saves the changes to the database
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+
+            _db.DiaryEntries.Remove(obj);  // Remove the diary entry from the database
+            _db.SaveChanges();// Saves the changes to the database
             return RedirectToAction("Index");
+
         }
     }
 }
--- Diary/DiaryApp/Models/DiaryEntry.cs
+++ Diary/DiaryApp/Models/DiaryEntry.cs
@@ -7,7 +7,8 @@ namespace DiaryApp.Models
         // Properties
         //[Key] 
         public int DiaryEntryId { get; set; }
-        [Required] // cannot be null
+        [Required(ErrorMessage = "Please enter a title!!! (Client side validation)")] // cannot be null
+        [StringLength(50,MinimumLength = 2, ErrorMessage = "Title must be between 2 and 50 Characters!!! (Client side validation)")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;
