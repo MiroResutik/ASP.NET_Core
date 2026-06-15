4d38a72b8f759c5b1f818ac61791663d13e5f156
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-15 15:56:27 +0100

    Data - Role, User Seeder class added. Controllers - Roles and Home controller

---
+++ Diary/DevSpot/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DevSpot.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DevSpot.Data
+{
+    // Identiy will allow to create users with specific roles
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<JobPosting> JobPostings { get; set; }
+
+        // Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Diary/DevSpot/Data/RoleSeeder.cs
@@ -0,0 +1,28 @@
+﻿using DevSpot.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevSpot.Data
+{
+    public class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManger = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!await roleManger.RoleExistsAsync(Roles.Admin))
+            {
+                await roleManger.CreateAsync(new IdentityRole(Roles.Admin));
+            }
+
+            if (!await roleManger.RoleExistsAsync(Roles.JobSeeker))
+            {
+                await roleManger.CreateAsync(new IdentityRole(Roles.JobSeeker));
+            }
+
+            if (!await roleManger.RoleExistsAsync(Roles.Employer))
+            {
+                await roleManger.CreateAsync(new IdentityRole(Roles.Employer));
+            }
+        }
+    }
+}
