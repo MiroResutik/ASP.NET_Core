e18730e31be82bffd345aa4211b74a620601cf13
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-15 15:58:00 +0100

    Revert "Migrations to database"
    This reverts commit 811ccad8f0dc3ce359cbffb744be6685d93db015.

--- Diary/DevSpot/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevSpot.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
