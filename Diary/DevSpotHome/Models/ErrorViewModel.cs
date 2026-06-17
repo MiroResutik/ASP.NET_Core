b1f58d74075cd556b61659e130e9237bc11b9a07
miroslav.resutik@hotmail.co.uk <miroslav.resutik@hotmail.co.uk>
2026-06-17 21:04:46 +0100

    DevSpotHome

---
+++ Diary/DevSpotHome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevSpotHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
