028349c03605f57e5370bd27261bd4aa7281c015
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-18 12:18:21 +0100

    Layouts and CSS

--- Diary/BlazorApp/Data/Grocery.cs
+++ Diary/BlazorApp/Data/Grocery.cs
@@ -1,9 +1,14 @@
-﻿namespace BlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorApp.Data
+
 {
     public class Grocery
     {
+        [Required] // Form validation
+        [StringLength(15, ErrorMessage ="Name should be less than 15 characters!!!")]
         public string Name { get; set; }
-
+        [Required] // Form validation
+        [Range(1, 1000, ErrorMessage = "Valid price range is between 1 - 1000!!!")]
         public float Price { get; set; }
 
     }
