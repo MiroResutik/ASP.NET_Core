7fbf48e9a38f9c34308277015d45e36c5dd98f54
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-18 16:36:27 +0100

    Loading the data from HTTP json request and displaying it

--- Diary/BlazorApp/Program.cs
+++ Diary/BlazorApp/Program.cs
@@ -10,10 +10,23 @@ namespace BlazorApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // Http call to URL
+            builder.Services.AddScoped(
+                sp => new HttpClient
+                {
+                    BaseAddress = new Uri("https://api.open-meteo.com/v1/")
+                });
 
             await builder.Build().RunAsync();
+
+            // HTTP call is Base address of the project application
+            /*
+            builder.Services.AddScoped(
+                sp => new HttpClient { 
+                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+            */
         }
     }
 }
