3c93c85909b7f2b0d5b2454b635d12665006ece9
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-06-19 11:06:31 +0100

    Blazor File Upload App

---
+++ Diary/BlazorFileUpload/Services/FileUpload.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorFileUpload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+
+        private  IWebAssemblyHostEnvironment _webHostEnviroment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebAssemblyHostEnvironment webAssemblyHostEnvironment, 
+            ILogger<FileUpload> logger)
+        {
+            _logger = logger;
+            _webHostEnviroment = webAssemblyHostEnvironment;
+        }
+
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if (file != null) {
+                try
+                {
+
+                    var uploadPath = Path.Combine(_webHostEnviroment.BaseAddress, "uploads", file.Name);
+
+                    using (var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Diary/BlazorFileUpload/Services/IWebHostEnvironment.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorFileUpload.Services
+{
+    internal interface IWebHostEnvironment
+    {
+    }
+}
